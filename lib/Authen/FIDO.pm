0ede6a446041e280911a0365a857767e25d45ef3
Jacques Germishuys <jacquesg@striata.com>
2020-05-15 21:14:10 +0100

    WIP

---
+++ lib/Authen/FIDO.pm
@@ -0,0 +1,38 @@
+package Authen::FIDO;
+
+use strict;
+use warnings;
+use Carp;
+
+1;
+
+__END__
+
+=for HTML
+<a href="https://dev.azure.com/jacquesgermishuys/p5-Authen-FIDO/_build">
+	<img src="https://dev.azure.com/jacquesgermishuys/p5-Authen-FIDO/_apis/build/status/jacquesg.p5-Authen-FIDO?branchName=master" alt="Build Status: Azure Pipeline" align="right" />
+</a>
+<a href="https://coveralls.io/r/jacquesg/p5-Authen-FIDO">
+	<img src="https://coveralls.io/repos/github/jacquesg/p5-Authen-FIDO/badge.svg?branch=master" alt="coveralls" align="right" />
+</a>
+=cut
+
+=head1 NAME
+
+Authen::FIDO - FIDO Authentication
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Jacques Germishuys <jacquesg@striata.com>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2020 Jacques Germishuys.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
